fe0578a3e998ef3cc5e1e33ffc029bd963e88a76
Quyen Tat <tvq9612@gmail.com>
2023-06-09 12:25:27 +0700

    feat: style code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,17 +10,17 @@ import { Analytics } from '@vercel/analytics/react'
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
 import posthog from 'posthog-js'
+// this might be better for dark mode
+import 'prismjs/themes/prism-tomorrow.css'
 // used for code syntax highlighting (optional)
-import 'prismjs/themes/prism-coy.css'
+// import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
-// this might be better for dark mode
-// import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
-import 'styles/prism-theme.css'
+// import 'styles/prism-theme.css'
 import 'styles/zcustom.scss'
 
 import { bootstrap } from '@/lib/bootstrap-client'
